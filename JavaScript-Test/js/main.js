c0c537aaa1b6a1f0edb7319b2a1e32df77bba898
ManuelAtlantaTorres <joseatlantatorres@gmail.com>
2016-09-21 00:43:35 -0500

    Add files via upload

---
+++ JavaScript-Test/js/main.js
@@ -0,0 +1,18 @@
+
+
+// alert ("esto es una alerta");
+// promt("Dame un numero: ");
+
+var edad = prompt("Dime tu edad chinga");
+
+console.log("La edad del usuario es:" + edad);
+// console.log ("La edad del usatrio")
+
+if (edad >= 18) {
+	alert("el usuario es mayor de edad");
+} else{
+
+	alert("el usario no es mayor de edad wey! ")
+
+}
+
